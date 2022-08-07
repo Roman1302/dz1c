098e3f566b9d1b9da03875fc8c8a1834e4f6221a
Роман Бухмистов <rrb13@mail.ru>
2022-08-07 11:02:30 +0300

    ДЗ №1 по C# c дополнением проверяющего

--- task8/Program.cs
+++ task8/Program.cs
@@ -3,8 +3,12 @@ Console.WriteLine("Программа вывода четных чисел до 
 Console.Write("Введите число: ");
 int a = int.Parse(Console.ReadLine());
 int n = 0; 
-    for (int i = 0; i < a/2; i++)
-        {
-            Console.Write($"{n = n+2} ");
-        }
+for (int i = 2; i <= a; i = i+2) //препод
+{
+Console.Write($"{i} "); //препод
+}
+    // for (int i = 0; i < a/2; i++)
+    //     {
+    //         Console.Write($"{n = n+2} ");
+        //}
     Console.ReadKey();
